a02deff6d209f7337538e235513686399559e7a6
steciuk <a.t.steciuk@gmail.com>
2024-09-03 19:30:38 +0200

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    react(),
+    sitemap(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+  site: "https://gia.steciuk.dev",
+});
